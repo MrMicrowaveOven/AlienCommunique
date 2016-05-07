312bed83e694791a12761227b34ac760972dfcff
Benjamin <benj@zagorski.com>
2016-05-07 13:48:52 -0700

    AddedInitialBinaryDecoder

--- lib/initial_decoding.rb
+++ lib/initial_decoding.rb
@@ -1,3 +1,22 @@
-def binary_to_decimal
+def binary_to_decimal(string)
+  # Convert to string if not already
+  string = string.to_s
+  array = string.chars
+  array.each do |char|
+    char = char.to_i
+  end
 
+  total = 0
+  digit = string.length
+
+  # Start at right
+  array.each do |int|
+    if int == 1
+      total += 2 ** digit
+    end
+    digit -= 1
+  end
+  # start at last digit, i = 0
+  # If 1, add 2^1
+  total
 end
