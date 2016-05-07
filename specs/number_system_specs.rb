444de945c346e28d57bc5312e300734fd1e98b70
Benjamin <benj@zagorski.com>
2016-05-07 15:25:39 -0700

    NumberSystemFirstSpecs

--- specs/number_system_specs.rb
+++ specs/number_system_specs.rb
@@ -1,11 +1,10 @@
 require_relative 'spec_helper.rb'
 
-describe "#get_number_array" do
-  it "converts basic binary" do
-    expect(binary_to_decimal(101)).to eq(5)
+describe "#convert_binary" do
+  it "converts a line of spaces and Xs into a number" do
+    expect(convert_binary("X  X")).to eq(9)
   end
   it "converts basic non-symmetric binary" do
-    expect(binary_to_decimal(1011)).to eq(11)
+    expect(binary_to_decimal("X XX")).to eq(11)
   end
-
 end
