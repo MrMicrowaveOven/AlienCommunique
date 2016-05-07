2d7ec373da1463b500ef2823477a4664470939e3
Brian Hayzlett <brianhayzlett@yahoo.com>
2016-05-07 14:58:15 -0700

    double x and double space for imagify

--- imagify.rb
+++ imagify.rb
@@ -2,6 +2,6 @@ message = File.readlines 'SETI_message.txt'
 
 
 message[0].each_char.with_index do |chr, idx|
-  print ((chr === '0') ? ' ' : '.')
   puts if idx % 359 == 0
+  print ((chr === '0') ? '  ' : 'XX')
 end
