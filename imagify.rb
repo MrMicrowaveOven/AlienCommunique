3e5ffebe3c3ebc0ec88d96d0a1faf72e5d738d86
medubin <medubin@gmail.com>
2016-05-07 15:30:09 -0700

    changed imagify

--- imagify.rb
+++ imagify.rb
@@ -3,7 +3,7 @@ message = File.readlines 'SETI_message.txt'
 translation = File.open("translation.txt", "w")
 message[0].each_char.with_index do |chr, idx|
   translation.puts if idx % 359 == 0 && idx != 0
-  translation.print ((chr === '0') ? ' ' : 'X')
+  translation.print ((chr === '0') ? '  ' : 'XX')
 end
 
 translation.close
