405a747f258001f70bec89a65abb63c13d112607
kithaca <kithaca@gmail.com>
2016-05-07 17:09:23 -0700

    fix decoding

--- lib/decode_msg.rb
+++ lib/decode_msg.rb
@@ -2,17 +2,12 @@ require_relative 'initial_decoding'
 
 messages = File.readlines('../messages.txt')
 
-adjusted = []
 messages.each do |line|
 	if line.start_with?("0") || line.start_with?("1")
-		adjusted << line
+		puts binary_to_decimal(line.reverse.to_i)
+	else
+		puts line
 	end
-end
-
-adjusted.each do |line|
-	puts binary_to_decimal(line.to_i)
-	puts binary_to_decimal(line.reverse.to_i)
-	puts
 end
 
 
